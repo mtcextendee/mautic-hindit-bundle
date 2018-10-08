5857cda325ad6082ea7c518f28bf918530be80d1
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-10-08 17:24:44 +0200

    Fix set 0 for required keys

--- Integration/HinditIntegration.php
+++ Integration/HinditIntegration.php
@@ -13,6 +13,8 @@
 namespace MauticPlugin\MauticHinditBundle\Integration;
 
 use Mautic\PluginBundle\Integration\AbstractIntegration;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * Class HinditIntegration.
@@ -50,9 +52,58 @@ class HinditIntegration extends AbstractIntegration
             'loginID'  => 'mautic.plugin.hindit.login.id',
             'password' => 'mautic.plugin.hindit.password',
             'senderid' => 'mautic.plugin.hindit.sender.id',
-            'Unicode' => 'mautic.plugin.hindit.unicode',
             'route_id' => 'mautic.plugin.hindit.route.id',
         ];
+    }
+
+    /**
+     * @param \Mautic\PluginBundle\Integration\Form|FormBuilder $builder
+     * @param array                                             $data
+     * @param string                                            $formArea
+     */
+    public function appendToForm(&$builder, $data, $formArea)
+    {
+        if ($formArea == 'keys') {
+
+            /* @var FormBuilder $builder */
+            $builder->add(
+                'Unicode',
+                TextType::class,
+                [
+                    'label'       => 'mautic.plugin.hindit.unicode',
+                    'required'    => true,
+                    'attr'        => [
+                        'class' => 'form-control',
+                    ],
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'mautic.core.value.required',
+                            ]
+                        ),
+                    ],
+                ]
+            );
+
+            $builder->add(
+                'route_id',
+                TextType::class,
+                [
+                    'label'       => 'mautic.plugin.hindit.route.id',
+                    'required'    => true,
+                    'attr'        => [
+                        'class' => 'form-control',
+                    ],
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'mautic.core.value.required',
+                            ]
+                        ),
+                    ],
+                ]
+            );
+        }
     }
 
     /**
